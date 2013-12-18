9830dace73d6f0b1c01487d7ed15ec40573d341e
Stefan Bertilsson <smuffi@hotmail.se>
2013-12-18 12:18:47 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="wrapper">
+
+	<nav>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="socks.php">Socks</a></li>
+		<li><a href="jackets.php">Jackets</a></li>
+		<li><a href="t-shirts.php">T-Shirts</a></li>
+		<li><a href="headwears.php">Headwear</a></li>
+	</nav>
+
+	<section id="content">
