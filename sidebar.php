97dc14630734bfcb4d4146daa302e6ffa18b4b0a
Stefan Bertilsson <smuffi@hotmail.se>
2013-12-18 14:55:41 +0100

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -7,6 +7,7 @@
 
 			while(!feof($file)){
 
+				// Write get the lines from order.txt and put them into separated variables to be used later.
 				$type = fgets($file);
 				$value = fgets($file);
 				$number = fgets($file);
@@ -15,15 +16,18 @@
 				$price = $number * $value;
 				$totalPrice = $totalPrice + $price;
 
+				// use the variables to print out a decent looking cart.
 				echo $number . "x $color $type". "size: $size " . $price . "kr<br />";
 			}
 
+			// Print the total price of all our wares in the cart.
 			echo "$totalPrice" . "kr";
 			fclose($file);
 
 			echo "<form action=\"index.php\" method=\"post\"><input type=\"submit\" name=\"checkout\" value=\"checkout\"><input type=\"submit\" name=\"reset\" value=\"Remove Order\"></form>";
 
 		}else{
+			// Incase the cart is empty / non-existant.
 			echo "You have nothing in your order.";
 		}
 
