58a968cdf395de491856c903bfe860ec5c174876
Stefan Bertilsson <smuffi@hotmail.se>
2013-12-18 12:18:07 +0100

    Create headwears.php

---
+++ headwears.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'; ?>
+
+	<form action="headwear.php" method="post">
+		<select name="headwear-type">
+			<option value="Tophat">Tophats</option>
+			<option value="Cap">Caps</option>
+		</select><br />
+		<input type="submit" name="headwear" value="Next">
+	</form>
+
+</section><!-- #content -->
+
+<?php include 'sidebar.php'; ?>
+
+</div><!-- #wrapper -->
+</body>
+</html>
