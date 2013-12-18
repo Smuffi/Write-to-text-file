3b060a9d72430492fb532a201c5a6a0a724cff59
Stefan Bertilsson <smuffi@hotmail.se>
2013-12-18 12:17:04 +0100

    Create jackets.php

---
+++ jackets.php
@@ -0,0 +1,19 @@
+<?php include 'header.php'; ?>
+
+	<form action="jacket.php" method="post">
+		<select name="jacket-type">
+			<option value="Rainbow Dash">Rainbow Dash</option>
+			<option value="Wonderbolts">Wonderbolts</option>
+			<option value="Nascar">Nascar</option>
+			<option value="Lord of the Ring">Lord of the Ring</option>
+		</select><br />
+		<input type="submit" name="jacket" value="Next">
+	</form>
+
+</section><!-- #content -->
+
+<?php include 'sidebar.php'; ?>
+
+</div><!-- #wrapper -->
+</body>
+</html>
